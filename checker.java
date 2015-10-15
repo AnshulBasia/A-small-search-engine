e97d7060917ed0fa433bb74c8c1ab56711175947
Anshul Basia <mt6140502@iitd.ac.in>
2015-10-15 19:57:01 +0530

    Create checker.java

---
+++ checker.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+
+public class checker
+{
+	public static void main ( String args [])
+	{
+		SearchEngine r = new SearchEngine();
+		BufferedReader br = null;try {
+			String actionString;
+			br = new BufferedReader(new FileReader("actions.txt"));
+
+			while ((actionString = br.readLine()) != null) {
+				r.performAction(actionString);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+
+		
+
+	}
+}
